5989ceff51b8a0ad22653c8996aa2223cb12328e
Goldenbeasty <goldenbeaster@gmail.com>
2023-02-27 17:13:04 +0200

    Added a POC for config

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -26,6 +26,39 @@ type database struct {
 	DBName   string `toml:"dbname"`
 }
 
+// POC, needs to be replaced with a not so bad function
+func envCheckVariable(cfg configuration) configuration {
+
+	driver, ok := os.LookupEnv("POSTGRES_PASSWORD")
+	if !ok {
+		log.Println("WARNING no enviroment variable set for POSTGRES_PASSWORD, reading from configuration file")
+	} else {
+		cfg.Database.Password = driver
+	}
+	driver, ok = os.LookupEnv("POSTGRES_DATABASE")
+	if !ok {
+		log.Println("WARNING no enviroment variable set for POSTGRES_DATABASE, reading from configuration file")
+	} else {
+		cfg.Database.DBName = driver
+	}
+	driver, ok = os.LookupEnv("POSTGRES_USER")
+	if !ok {
+		log.Println("WARNING no enviroment variable set for POSTGRES_USER, reading from configuration file")
+	} else {
+		cfg.Database.User = driver
+	}
+
+	driver, ok = os.LookupEnv("BACKEND_CORS_EXTRA_ALLOWED_ORIGIN")
+	if !ok {
+		log.Println("WARNING no enviroment variable set for BACKEND_CORS_EXTRA_ALLOWED_ORIGIN, reading from configuration file")
+	} else {
+		cfg.Web.CORSAllowedOrigins = append(cfg.Web.CORSAllowedOrigins, driver)
+	}
+
+	log.Println(cfg)
+	return cfg
+}
+
 func parseConfig() configuration {
 	// default config
 	cfg := configuration{
@@ -56,6 +89,8 @@ func parseConfig() configuration {
 	if err != nil {
 		panic(err)
 	}
+
+	cfg = envCheckVariable(cfg)
 
 	return cfg
 }
