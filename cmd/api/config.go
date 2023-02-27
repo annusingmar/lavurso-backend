3a3890d3677445e29912e4484c7d60a0ca91e900
annusingmar <108177565+annusingmar@users.noreply.github.com>
2023-02-28 00:21:24 +0200

    finish reading environment variables

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
 
-// configuration sisaldab konfigureeritavaid vaartusi, enamasti config.toml file'i kaudu
 type configuration struct {
 	Web      web      `toml:"web"`
 	Database database `toml:"database"`
@@ -23,40 +25,7 @@ type database struct {
 	Port     int    `toml:"port"`
 	User     string `toml:"user"`
 	Password string `toml:"password"`
-	DBName   string `toml:"dbname"`
-}
-
-// POC, needs to be replaced with a not so bad function
-func envCheckVariable(cfg configuration) configuration {
-
-	driver, ok := os.LookupEnv("POSTGRES_PASSWORD")
-	if !ok {
-		log.Println("WARNING no enviroment variable set for POSTGRES_PASSWORD, reading from configuration file")
-	} else {
-		cfg.Database.Password = driver
-	}
-	driver, ok = os.LookupEnv("POSTGRES_DATABASE")
-	if !ok {
-		log.Println("WARNING no enviroment variable set for POSTGRES_DATABASE, reading from configuration file")
-	} else {
-		cfg.Database.DBName = driver
-	}
-	driver, ok = os.LookupEnv("POSTGRES_USER")
-	if !ok {
-		log.Println("WARNING no enviroment variable set for POSTGRES_USER, reading from configuration file")
-	} else {
-		cfg.Database.User = driver
-	}
-
-	driver, ok = os.LookupEnv("BACKEND_CORS_EXTRA_ALLOWED_ORIGIN")
-	if !ok {
-		log.Println("WARNING no enviroment variable set for BACKEND_CORS_EXTRA_ALLOWED_ORIGIN, reading from configuration file")
-	} else {
-		cfg.Web.CORSAllowedOrigins = append(cfg.Web.CORSAllowedOrigins, driver)
-	}
-
-	log.Println(cfg)
-	return cfg
+	Name     string `toml:"dbname"`
 }
 
 func parseConfig() configuration {
@@ -71,26 +40,80 @@ func parseConfig() configuration {
 			Port:     5432,
 			User:     "username",
 			Password: "password",
-			DBName:   "database_name",
+			Name:     "database_name",
 		},
 	}
 
 	configData, err := os.ReadFile("config.toml")
 	if err != nil {
 		if os.IsNotExist(err) {
-			log.Println("WARNING config doesn't exist, using default values")
-			return cfg
+			log.Println("INFO config doesn't exist, default values will be used if environment variables aren't set")
 		} else {
+			panic(err)
+		}
+	} else {
+		log.Println("INFO config exists, but environment variables will override values set in config")
+		err = toml.Unmarshal(configData, &cfg)
+		if err != nil {
 			panic(err)
 		}
 	}
 
-	err = toml.Unmarshal(configData, &cfg)
-	if err != nil {
-		panic(err)
-	}
+	cfg.checkEnvironment()
 
-	cfg = envCheckVariable(cfg)
+	fmt.Println(cfg)
 
 	return cfg
+}
+
+func (cfg *configuration) checkEnvironment() {
+	val, ok := os.LookupEnv("WEB_LISTEN")
+	if ok {
+		log.Println("INFO using environment variable WEB_LISTEN")
+		cfg.Web.Listen = val
+	}
+
+	val, ok = os.LookupEnv("WEB_CORS_ALLOWED_ORIGINS")
+	if ok {
+		log.Println("INFO using environment variable WEB_CORS_ALLOWED_ORIGINS")
+		list := strings.Split(val, ",")
+		for i, v := range list {
+			list[i] = strings.TrimSpace(v)
+		}
+		cfg.Web.CORSAllowedOrigins = list
+	}
+
+	val, ok = os.LookupEnv("DATABASE_HOST")
+	if ok {
+		log.Println("INFO using environment variable DATABASE_HOST")
+		cfg.Database.Host = val
+	}
+
+	val, ok = os.LookupEnv("DATABASE_PORT")
+	if ok {
+		log.Println("INFO using environment variable DATABASE_PORT")
+		port, err := strconv.Atoi(val)
+		if err != nil {
+			log.Println("ERROR failed reading environment variable DATABASE_PORT, skipping it")
+		}
+		cfg.Database.Port = port
+	}
+
+	val, ok = os.LookupEnv("DATABASE_USER")
+	if ok {
+		log.Println("INFO using environment variable DATABASE_USER")
+		cfg.Database.User = val
+	}
+
+	val, ok = os.LookupEnv("DATABASE_PASSWORD")
+	if ok {
+		log.Println("INFO using environment variable DATABASE_PASSWORD")
+		cfg.Database.Password = val
+	}
+
+	val, ok = os.LookupEnv("DATABASE_NAME")
+	if ok {
+		log.Println("INFO using environment variable DATABASE_NAME")
+		cfg.Database.Name = val
+	}
 }
